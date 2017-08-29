408c16b896a5b57737876df345a0823a271aa38b
Campbell Barton <ideasman42@gmail.com>
2017-08-30 07:03:17 +1000

    Initial commit

---
+++ content/conf.py
@@ -0,0 +1,67 @@
+
+import sys, os
+
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'exts')))
+
+extensions = [
+    # general
+    'youtube',
+    'newsfeed',
+
+    'sphinx.ext.extlinks',
+]
+extlinks = {
+    'task': ('http://developer.blender.org/T%s', 'T'),
+    'rev': ('http://developer.blender.org/r%s', 'r'),
+    'diff': ('http://developer.blender.org/D%s', 'D'),
+}
+
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+exclude_patterns = ['template.rst']
+master_doc = 'index'
+
+# General information about the project.
+project = 'This Week in Blender Development'
+copyright = 'Creative Commons'
+
+# without this it calls it 'documentation', which it's not
+html_title = 'This Week in Blender Development'
+html_short_title = 'This Week in Blender Dev'
+
+# visual noise for my purpose
+html_show_copyright = False
+html_show_sphinx = False
+html_show_sourcelink = False
+
+'''
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    sphinx_rtd_theme = None
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+if sphinx_rtd_theme:
+    html_theme = 'sphinx_rtd_theme'
+else:
+    html_theme = 'haiku'
+
+if sphinx_rtd_theme:
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+'''
+
+html_theme = 'alabaster'
+html_theme_options = {
+    "show_powered_by": False,
+}
+import alabaster
+print(alabaster.__file__)
+html_theme_path = [alabaster.get_path()]
+
+
+# for our own extensions!
+phabricator_base = 'http://developer.blender.org'
+
